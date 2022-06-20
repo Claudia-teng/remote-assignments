83b37ba9daa6287301b32a67b88552c6d5555cbe
Claudia-teng <sydeng102@gmail.com>
2022-06-20 14:35:42 +0800

    add week 3 assignments

---
+++ Week-3/Assignment-5/algorithm-practice.js
@@ -0,0 +1,25 @@
+function twoSum1(nums, target) {
+  for (let i=0; i<nums.length-1; i++) {
+    for (let j=i+1; j<nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+function twoSum2(nums, target) {
+  let map = {};
+
+  for (let i=0; i<nums.length; i++) {
+    let goal = target - nums[i];
+    if (map[goal]) {
+      return [i, Number(map[goal])];
+    } else {
+      map[nums[i]] = String(i)
+    }
+  }
+}
+
+twoSum1([2, 7, 11, 15], 9)
+twoSum2([2, 7, 11, 15], 9)
