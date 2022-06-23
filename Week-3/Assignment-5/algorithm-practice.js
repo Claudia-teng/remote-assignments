3778cfa3517607e371df237959a49e4cfe160e67
Claudia-teng <sydeng102@gmail.com>
2022-06-23 12:32:13 +0800

    prettier format

--- Week-3/Assignment-5/algorithm-practice.js
+++ Week-3/Assignment-5/algorithm-practice.js
@@ -1,6 +1,6 @@
 function twoSum1(nums, target) {
-  for (let i=0; i<nums.length-1; i++) {
-    for (let j=i+1; j<nums.length; j++) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
@@ -11,15 +11,15 @@ function twoSum1(nums, target) {
 function twoSum2(nums, target) {
   let map = {};
 
-  for (let i=0; i<nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let goal = target - nums[i];
     if (map[goal]) {
       return [i, Number(map[goal])];
     } else {
-      map[nums[i]] = String(i)
+      map[nums[i]] = String(i);
     }
   }
 }
 
-twoSum1([2, 7, 11, 15], 9)
-twoSum2([2, 7, 11, 15], 9)
+twoSum1([2, 7, 11, 15], 9);
+twoSum2([2, 7, 11, 15], 9);
--- Week-3/Assignments/server.js
+++ Week-3/Assignments/server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = 3000;
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  return res.sendFile('index.html', path.join(__dirname, 'public'))
-})
+app.get("/", (req, res) => {
+  return res.sendFile("index.html", path.join(__dirname, "public"));
+});
 
-app.get('/data', (req, res) => {
+app.get("/data", (req, res) => {
   let number = req.query.number;
   if (!number) {
     return res.status(400).json({
-      error: 'Lack of Parameter'
+      error: "Lack of Parameter",
     });
   }
 
@@ -24,48 +24,47 @@ app.get('/data', (req, res) => {
 
   if (isNaN(number)) {
     return res.status(400).json({
-      error: 'Wrong Parameter'
+      error: "Wrong Parameter",
     });
   }
 
   function count(number) {
     if (number < 1) return 0;
-    return number + count(number-1);
+    return number + count(number - 1);
   }
 
   let answer = count(number);
 
   return res.status(200).json({
-    answer
+    answer,
   });
 });
 
-app.get('/myName', (req, res) => {
+app.get("/myName", (req, res) => {
   let name = req.cookies.name;
   if (name) {
     return res.status(200).send(`
       <h1>Name:</h1>  
       <h1>${name}</h1>
-      `
-    )
+      `);
   } else {
-    res.redirect('/trackname.html')
+    res.redirect("/trackname.html");
   }
-})
+});
 
-app.get('/trackName', (req, res) => {
+app.get("/trackName", (req, res) => {
   const name = req.query.name;
   if (!name) {
     return res.status(400).json({
-      error: 'Name is required.'
-    })
+      error: "Name is required.",
+    });
   }
-  res.cookie('name', name, { maxAge: 1000 * 60 * 60, httpOnly: true });
+  res.cookie("name", name, { maxAge: 1000 * 60 * 60, httpOnly: true });
   return res.status(200).json({
-    ok: true
-  })
-})
+    ok: true,
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`)
+  console.log(`Listening on ${PORT}...`);
 });
